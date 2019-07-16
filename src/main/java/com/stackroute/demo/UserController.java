afc70696d3728a96ee4015a96089cb376c6327d5
Arpita <arpita.mazumder02@gmail.com>
2019-07-16 15:02:00 +0530

    Committing the Exercise 1 of Spring MVC

--- src/main/java/com/stackroute/demo/UserController.java
+++ src/main/java/com/stackroute/demo/UserController.java
@@ -3,15 +3,16 @@ package com.stackroute.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-    @Controller
+@Controller
     public class UserController {
-        @RequestMapping("/click")
+        @RequestMapping(value = "/",method = RequestMethod.GET)
         public String redirect(Model m) {
             User user = new User();
             user.setMessage("Welcome to Stackroute.");
             m.addAttribute("message",user.getMessage());
 
-            return "viewpage";
+            return "index";
         }
     }
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<a href="click">Click here</a>
-</body>
-</html>
